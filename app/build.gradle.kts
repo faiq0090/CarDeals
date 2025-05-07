361b37e485349992818b8387fed76d336deabbe1
shaffin <i212963@nu.edu.pk>
2025-05-07 14:10:24 +0500

    volley dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("com.android.volley:volley:1.2.1")
+
     //  implementation(project(":app"))
     implementation ("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.credentials:credentials:1.5.0")
